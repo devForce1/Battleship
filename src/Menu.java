63b68f017a396ea678d337ce65cd207101d03bd5
RobAnd90 <robin.andersson@edu.edugrade.se>
2022-11-03 22:55:54 +0100

    Added code to Menu Added Background Added Buttons

--- src/Menu.java
+++ src/Menu.java
@@ -1,9 +1,14 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Menu extends Application {
@@ -11,6 +16,10 @@ public class Menu extends Application {
     Stage window;
     Button startButton;
     Button exitButton;
+    ImageView imageView;
+
+    HBox hBox = new HBox();
+    Image image = new Image(getClass().getResourceAsStream("BattleShip.png"));
 
     public static void main(String[] args) {
         launch(args);
@@ -20,6 +29,7 @@ public class Menu extends Application {
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
         window.setTitle("Battle Ship");
+        Group root = new Group();
 
         startButton = new Button("Start");
         startButton.setOnAction(e -> {});
@@ -27,9 +37,27 @@ public class Menu extends Application {
         exitButton = new Button("Exit");
         exitButton.setOnAction(e -> {});
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(startButton);
-        Scene scene = new Scene(layout, 1200, 800);
+        hBox.setSpacing(50);
+        hBox.getChildren().add(startButton);
+        hBox.getChildren().add(exitButton);
+        hBox.setPadding(new Insets(400, 200, 200, 500));
+        startButton.setMinWidth(150);
+        startButton.setMaxWidth(150);
+        startButton.setMinHeight(20);
+        startButton.setMaxHeight(50);
+        exitButton.setMinWidth(150);
+        exitButton.setMaxWidth(150);
+        exitButton.setMinHeight(20);
+        exitButton.setMaxHeight(50);
+
+
+        VBox VBox = new VBox();
+        VBox.getChildren().add(hBox);
+        VBox.setAlignment(Pos.CENTER);
+        BackgroundSize size = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true);
+        Background background = new Background(new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, size));
+        VBox.setBackground(background);
+        Scene scene = new Scene(VBox, 1300, 1200);
         window.setScene(scene);
         window.show();
     }
