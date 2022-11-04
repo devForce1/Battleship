f1de2abde74d705f1dc9111cf3f4416593d8cf35
RobAnd90 <robin.andersson@edu.edugrade.se>
2022-11-04 19:13:25 +0100

    Added a ConfirmBox class Added a GridPane Class
    ConfirmBox class have code that is finished and working.
    
    Added function to Exit button in Menu class

---
+++ src/ConfirmBox.java
@@ -0,0 +1,46 @@
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
+
+public class ConfirmBox {
+
+    static boolean yesNo;
+
+    public static boolean display(String titel, String message) {
+        Stage window = new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL); //Blocks from pressing outside the window
+        window.setTitle(titel);
+        window.setMinWidth(250);
+        Label label = new Label();
+        label.setText(message);
+
+        Button yesButton = new Button("Yes");
+        Button noButton = new Button("No");
+
+        yesButton.setOnAction(e -> {
+            yesNo = true;
+            window.close();
+        });
+
+        noButton.setOnAction(e -> {
+            yesNo = false;
+            window.close();
+        });
+
+        VBox vBox = new VBox(10);
+        vBox.getChildren().add(label);
+        vBox.getChildren().add(yesButton);
+        vBox.getChildren().add(noButton);
+        vBox.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(vBox);
+        window.setScene(scene);
+        window.showAndWait();
+
+        return yesNo;
+    }
+
+}
