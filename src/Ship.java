9935c4eb398d6810df43fbd00bfca74c58b17bba
Aleksandra Kisseljova <aleksandra.kisseljova@edu.edugrade.se>
2022-11-25 20:19:23 +0100

    Fixed change image

--- src/Ship.java
+++ src/Ship.java
@@ -37,7 +37,7 @@ public class Ship {
                 //change has ship to true
                 gameBoardCoordinates[i][ypos].setHasShip(true);
                 //switch image for this ship
- //               gameBoardCoordinates[i][ypos].changeImage();
+               gameBoardCoordinates[i][ypos].setCoordinateImageName("black.png");
             }
         }
         if(alignment == 2) {
@@ -45,25 +45,12 @@ public class Ship {
                 shipCoordinates.add(gameBoardCoordinates[i][xpos]);
                 gameBoardCoordinates[i][xpos].setOnCoordinate(this);
                 gameBoardCoordinates[i][xpos].setHasShip(true);
- //               gameBoardCoordinates[i][xpos].changeImage();
+                gameBoardCoordinates[i][xpos].setCoordinateImageName("black.png");
             }
         }
     }
 
-    //Method to check if the ship is sunken
-    public Boolean isSunken(){
-        //sets sunken to true
-        boolean sunken = true;
-        // for all coordinates of the specific ship
-        for (Coordinate coordinate : shipCoordinates){
-            //if no coordinates are hit -  sets sunken to false
-            if(!coordinate.getIsHit()){
-                sunken = false;
-            }
-        }
-        //otherwise returns true
-        return true;
-    }
+
 
     public static void createEnemyShips(){
         Ship a = new Ship(5,1,1,1,gameBoardCoordinates);
@@ -92,8 +79,19 @@ public class Ship {
         Ship p = new Ship(2,8,4,1,gameBoardCoordinates);
     }
 
-
-
-
+    //Method to check if the ship is sunken
+    public Boolean isSunken(){
+        //sets sunken to true
+        boolean sunken = true;
+        // for all coordinates of the specific ship
+        for (Coordinate coordinate : shipCoordinates){
+            //if no coordinates are hit -  sets sunken to false
+            if(!coordinate.getIsHit()){
+                sunken = false;
+            }
+        }
+        //otherwise returns true
+        return true;
+    }
 
 }
